57280d656a0f1cfd12453f9c5f735570a491a3eb
Ryomanba <datemanba3@gmail.com>
2021-03-15 14:44:16 +0900

    Lesson

---
+++ Lesson/Topic#6/Matrix_Vector_Multiplication.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+typedef unsigned long long int ull;
+#define BIG_NUM 2000000000
+#define HUGE_NUM 4000000000000000000
+#define MOD 1000000007
+#define EPS 0.000000001
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int a[n][m];
+    int b[m];
+    int ans[n] = {0};
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            ans[i] += a[i][j] * b[j];
+        }
+        cout << ans[i] << endl;
+    }
+    return 0;
+}
