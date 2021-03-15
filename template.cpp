afd22c1649b3566d5217549eddd8b114ff2e59e4
Ryomanba <datemanba3@gmail.com>
2021-03-15 14:44:42 +0900

    setting

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+typedef unsigned long long int ull;
+#define BIG_NUM 2000000000
+#define HUGE_NUM 4000000000000000000
+#define MOD 1000000007
+#define EPS 0.000000001
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
