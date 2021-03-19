7aca8bef12dee55b7480128268aa337a360f620b
Ryomanba <datemanba3@gmail.com>
2021-03-19 14:00:30 +0900

    Topic03

--- ALDS1/Topic03/Areas_on_the_Cross_Section_Diagram.cpp
+++ ALDS1/Topic03/Areas_on_the_Cross_Section_Diagram.cpp
@@ -8,6 +8,40 @@ typedef long long int ll;
 
 int main()
 {
-
-    return 0;
+     string s;
+     cin >> s;
+     // 水たまりの面積
+     deque<pair<int, int> > water;  
+     deque<int> deq;
+     for (int i = 0; i < s.size(); i++)
+     {
+         if (s[i] == '\\') deq.push_back(i);    // 下向きの場合
+         else if (s[i] == '/' && !deq.empty())  // 上向きかつ空じゃない場合
+         {
+             int num = deq.back();
+             deq.pop_back();
+             int area = i - num;
+             while (!water.empty() && num <= water.back().first)
+             {
+                area += water.back().second;
+                water.pop_back();
+             }
+             water.push_back(make_pair(num, area));
+         }
+     }
+     // 水たまりの総面積
+     int sum = 0;
+     int sz = water.size();
+     for (int i = 0; i < sz; i++)
+     {
+         sum += water[i].second;
+     }
+     cout << sum << endl;
+     cout << sz;
+     for (int i = 0; i < sz; i++)
+     {
+         cout << " " << water[i].second;
+     }
+     cout << endl;
+     return 0;
 }
