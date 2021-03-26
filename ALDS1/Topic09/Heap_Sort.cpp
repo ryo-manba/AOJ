0849d8974e3afae4143d0e8f5778e07b654b994d
Ryomanba <datemanba3@gmail.com>
2021-03-26 13:48:45 +0900

    Topic09

--- ALDS1/Topic09/Heap_Sort.cpp
+++ ALDS1/Topic09/Heap_Sort.cpp
@@ -6,8 +6,78 @@ typedef long long int ll;
 #define MOD 1e9 + 7
 #define EPS 1e-10
 
+/*  ヒープソート
+    1. 未整列データを木構造にする。
+    2. 木構造の根（ルート）が最大値または最小値になるように並び替えを行う。
+    3. 並び替えが完了した木構造の根（ルート）の値を整列済みデータとする。
+    4. 1～3を繰り返して、全て整列済みデータになったら終了。
+*/
+
+
+// i 番目の頂点を根とする部分木について、ヒープ条件を満たすようにする
+// a のうち 0 番目から N - 1 番目までの部分についてのみ考える
+
+void Heapify(vector<int> &a, int i, int N)
+{
+    int left_child = i * 2 + 1;
+    if (left_child >= N) return;
+
+    // 子供同士を比較
+    if (left_child + 1 < N && a[left_child + 1] > a[left_child])
+        ++left_child;
+    if (a[left_child] <= a[i]) return;
+
+    swap(a[i], a[left_child]);
+
+    // 再帰的に
+    Heapify(a, left_child, N);
+}
+
+void HeapSort(vector<int> &a)
+{
+    int N = (int)a.size();
+    // 全体をヒープにする
+    for (int i = N / 2 - 1; i >= 0; i--)
+        Heapify(a, i, N);
+    
+    // ヒープから 1 個 1 個最大値を pop する
+    for (int i = N - 1; i > 0; i--)
+    {
+        swap(a[0], a[i]);   // ヒープの最大値を左詰め
+        Heapify(a, 0, i);   // ヒープサイズを i に
+    }
+}
+
+// スワップの回数が最大になるように並び替える
+void inverse_HeapSort(vector<int> &a)
+{
+    for(int i = 2; i < (int)a.size(); i++)
+    {
+      swap(a[0], a[i - 1]);
+      int index = i;
+      while(index > 1)
+      {
+        swap(a[index - 1], a[index / 2 - 1]);
+        index /= 2;
+      }
+    }
+    int j = a.size();
+    swap(a[0], a[j - 1]);
+    return;
+}
+  
 int main()
 {
-
-    return 0;
+    int N;
+    cin >> N;
+    vector<int> a(N);
+    for (int i = 0; i < N; i++) cin >> a[i];
+    HeapSort(a);
+    inverse_HeapSort(a);
+    for (int i = 0; i < N; i++)
+    {
+        if (i) cout << " ";
+        cout << a[i];
+    }
+    cout << endl;
 }
--- ALDS1/Topic09/Priority_Queue.cpp
+++ ALDS1/Topic09/Priority_Queue.cpp
@@ -6,8 +6,70 @@ typedef long long int ll;
 #define MOD 1e9 + 7
 #define EPS 1e-10
 
+/*  優先順位キュー
+    キーと値をセットにして保持し、キーを基準としてある一定の優先順位をつける。
+    最も高い優先度を持つ要素をキューから取り除き、それを返す。
+*/
+
+const int SIZE = 2000100;
+int heap_size;
+
+void max_heapify(int *A, int i)
+{
+    int largest;
+    int left = i * 2 + 1;
+    int right = i * 2 + 2;
+
+    // 左の子、自分、右の子で値が最大のノードを選ぶ
+    if (left < heap_size && A[left] > A[i])
+        largest = left;
+    else
+        largest = i;
+    if (right < heap_size && A[right] > A[largest])
+        largest = right;
+
+    if (largest != i)   // i の子の方が大きい場合
+    {
+        swap(A[i], A[largest]); 
+        max_heapify(A, largest);    // 再帰的に呼び出す
+    }
+}
+
+void insert(int *A, int num)
+{
+    int i = heap_size++;
+    A[i] = num;
+    while (i && A[(i - 1) / 2] < A[i])
+    {
+        swap(A[(i - 1) / 2], A[i]);
+        i = (i - 1) / 2;
+    }
+}
+
+int extractMax(int *A)
+{
+    if (heap_size < 0) return 0;
+    int res = A[0];
+    A[0] = A[--heap_size];
+    max_heapify(A, 0);
+    return res;
+}
+
 int main()
 {
-
+    string s;
+    int A[SIZE];
+    while (cin >> s)
+    {
+        if (s == "end") break;
+        if (s == "insert")      // 要素を挿入
+        {
+            int num;
+            cin >> num;
+            insert(A, num);
+        }
+        if (s == "extract")     // 最大のキーを出力して削除
+            cout << extractMax(A) << endl;
+    }
     return 0;
 }
