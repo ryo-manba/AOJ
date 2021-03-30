ed1032b3452c6b5345e28a9ee3ef4b1896d050c0
Ryomanba <datemanba3@gmail.com>
2021-03-30 10:01:57 +0900

    Problems

---
+++ Problems/OOIIOII.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define INF 1e9
+#define INFL 1e18
+#define MOD 1e9 + 7
+#define EPS 1e-10
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 2, cnt = 1;
+    while (i - 1 <= n)
+    {
+        i *= 2;
+        cnt++;
+    }
+    cout << cnt - 1 << endl;
+    return 0;
+}
---
+++ Problems/ai1333.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define INF 1e9
+#define INFL 1e18
+#define MOD 1e9 + 7
+#define EPS 1e-10
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+int main()
+{
+    int x;
+    cin >> x;
+    string ans = "ai1333";
+    if (x != 0)
+    {
+        int tmp = x / 100;
+        while (tmp--)
+        {
+            ans += '3';
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
